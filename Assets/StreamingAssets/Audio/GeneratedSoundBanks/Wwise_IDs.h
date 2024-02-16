36ee2260881b829d276fb84d425990df4ca4540f
jaca2808 <29760330+jaca2808@users.noreply.github.com>
2024-02-16 06:42:59 +0100

    Jasmina - Wwise Integrated and added basic background music
    Jasmina - Wwise Integrated and added basic background music

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATES
+        {
+            static const AkUniqueID GROUP = 777429653U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME_STATE = 2678566748U;
+                static const AkUniqueID MAINMENU_STATE = 3205958645U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PAUSE_STATE = 1944582999U;
+            } // namespace STATE
+        } // namespace GAMESTATES
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC_SOUNDBANK = 3589812408U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
