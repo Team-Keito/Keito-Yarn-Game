79516c0f02a038dbd11ded1e5ca9fe729bf4ac50
Samuel325 <ayenisamuel87@gmail.com>
2024-02-16 15:17:33 -0500

    Added game manager and test cases
    Added some starter code for the game manager and also some test cases for scoring and counting the number of yarn balls.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score, numOfYarn;
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public int NumOfYarn
+    {
+        get { return numOfYarn; }
+        set { numOfYarn = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
