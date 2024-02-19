798a8d58ec4e9ac74ded01920ae331b91c44470b
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-02-19 15:47:40 -0800

    Moved Camera Switch & made an AutoFocus
    Updated:
    - Camera Switch to take optional target (Defaults to self).
      - Added changeable zoom level
    - Added Cinemachine to asmdef
    - made focal point smaller / recolored to be different from spawn point

---
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,36 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField, Tooltip("Default self")] 
+    private Transform _target;
+
+    [SerializeField, Tooltip("Default 0.5f")]
+    private float _zoomLevel = 0.5f;
+
+    private CinemachineFreeLook _camera;
+
+    private void Start()
+    {
+        if (!_camera)
+        {
+            _camera = FindObjectOfType<CinemachineFreeLook>();
+        }
+
+        if(!_target)
+        {
+            _target = this.transform;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        _camera.m_YAxis.Value = _zoomLevel;
+        _camera.Follow = _target;
+        _camera.LookAt = _target;
+        
+    }
+}
