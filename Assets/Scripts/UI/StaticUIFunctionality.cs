85a3f2493de4e49a8b381d36fa71c054fa2299ec
SurajSSingh <22825536+SurajSSingh@users.noreply.github.com>
2024-02-18 18:31:45 -0800

    Add Scene change to static functionality
    Can use UI Helper to change scene via build index or scene name

--- Assets/Scripts/UI/StaticUIFunctionality.cs
+++ Assets/Scripts/UI/StaticUIFunctionality.cs
@@ -8,6 +8,24 @@ using UnityEngine.SceneManagement;
 public class StaticUIFunctionality : MonoBehaviour
 {
     /// <summary>
+    /// Go to a scene via its build index
+    /// </summary>
+    /// <param name="buildIndex"></param>
+    public static void GoToSceneByIndex(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    /// <summary>
+    /// Go to a scene via its name
+    /// </summary>
+    /// <param name="buildIndex"></param>
+    public static void GoToSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
     /// Quit out of the game (will also stop editor).
     /// </summary>
     public static void QuitGame()
