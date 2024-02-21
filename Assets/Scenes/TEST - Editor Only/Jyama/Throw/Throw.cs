5d210a608ed0ea25bdd85fcc470f51ee7a0e7cf5
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-02-20 17:15:08 -0800

    Updated Line trace to hold

--- Assets/Scenes/TEST - Editor Only/Jyama/Throw/Throw.cs
+++ Assets/Scenes/TEST - Editor Only/Jyama/Throw/Throw.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class Throw : MonoBehaviour
+public class Throw : Base_InputSystem
 {
     [SerializeField] private GameObject _thrownPrefab;
     [SerializeField] private GameObject _indicator;
 
+    [Space(5)]
     [SerializeField] private float _force = 200f;
     [SerializeField] private Vector3 _offset;
 
+    [Space(5)]
     [SerializeField] private int _linePoints = 25;
     [SerializeField] private float _totalTime = 2f;
 
@@ -19,12 +21,11 @@ public class Throw : MonoBehaviour
 
     private float mass, drag;
     private GameObject current;
-    private bool isThrown = false;
 
-    private Vector3 startOffset => Camera.main.transform.position +
-        _offset.x * Camera.main.transform.forward +
-        _offset.y * Camera.main.transform.up +
-        _offset.z * Camera.main.transform.right;
+    private bool isHeld = false;
+    private Vector2 _orginalMousePos;
+
+    private Vector3 startOffset => CalcOffset(Camera.main.transform, _offset);
 
     private void Start()
     {
@@ -34,41 +35,59 @@ public class Throw : MonoBehaviour
 
         _lineRenderer = gameObject.GetComponent<LineRenderer>();
 
-        current = Instantiate(_thrownPrefab, startOffset, Quaternion.identity, Camera.main.transform);
-
-        DisableThrownObject(current);
-        UpdateLineColor(current);
+        _input.Player.Fire.started += Fire_started;
+        _input.Player.Fire.canceled += Fire_canceled;
     }
 
 
+    public Vector3 CalcOffset(Transform transform, Vector3 offset)
+    {
+        return transform.position +
+            offset.x * transform.forward +
+            offset.y * transform.up +
+            offset.z * transform.right;
+    }
+
+
+    private void Fire_started(InputAction.CallbackContext obj)
+    {
+        _orginalMousePos = Mouse.current.position.ReadValue();
+        isHeld = true;
+
+        _lineRenderer.enabled = true;
+        _indicator.SetActive(true);
+
+        Destroy(current);
+        SpawnNextThrownObject();
+        UpdateLineColor(current);
+    }
+
+    private void Fire_canceled(InputAction.CallbackContext obj)
+    {
+        isHeld = false;
+
+        EnableThrownObject(current);
+        ThrowItem(current);
+        
+        _lineRenderer.enabled = false;
+        _indicator.SetActive(false);
+    }
+
     private void Update()
     {
-        Vector2 mousePosition = Mouse.current.position.ReadValue();
+/*        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);*/
 
-        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (isHeld)
         {
-            if (isThrown)
-            {
-                isThrown = false;
-                current.GetComponent<Rigidbody>().mass = mass;
-            }
-            else
-            {
-                EnableThrownObject(current);
+            Vector2 mousePosition = Mouse.current.position.ReadValue() - _orginalMousePos;
 
-                ThrowItem(current);
-                Debug.Log(_lineRenderer.GetPosition(_lineRenderer.positionCount - 1));
-            }
-        }
-
-        DrawWithDrag();
-
-        _indicator.transform.position = _lineRenderer.GetPosition(_lineRenderer.positionCount - 1);
-
-        if (!isThrown)
+            //_force * Camera.main.transform.forward
+            
+            DrawWithDrag(_force * Camera.main.transform.forward);
+            _indicator.transform.position = _lineRenderer.GetPosition(_lineRenderer.positionCount - 1);
             current.transform.position = startOffset;
+        }      
     }
 
     private void UpdateLineColor(GameObject obj)
@@ -77,20 +96,14 @@ public class Throw : MonoBehaviour
         _lineRenderer.material.SetColor("_Color", color);
     }
 
-
     private void EnableThrownObject(GameObject go) 
     {
-        Collider collider = go.GetComponent<Collider>();
-        collider.enabled = true;
-
         go.transform.SetParent(null, true);
     }
-    private void DisableThrownObject(GameObject go)
+    private void SpawnNextThrownObject()
     {
-        Collider collider = go.GetComponent<Collider>();
-        collider.enabled = false;
-
-        go.transform.SetParent(Camera.main.transform, true);
+        current = Instantiate(_thrownPrefab, startOffset, Quaternion.identity, Camera.main.transform);
+        current.GetComponent<Rigidbody>().isKinematic = false;
     }
 
     private void ThrowItem(GameObject go)
@@ -100,18 +113,18 @@ public class Throw : MonoBehaviour
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
 
-        rb.AddForce( Camera.main.transform.forward * _force, ForceMode.Impulse);
+        rb.AddForce(Camera.main.transform.forward * _force, ForceMode.Impulse);
 
-        isThrown = true;
+        //isThrown = true;
     }
 
-    private void DrawWithDrag()
+    private void DrawWithDrag(Vector3 ForceVector)
     {
         _lineRenderer.positionCount = _linePoints;
         float time = _totalTime / _linePoints;
 
         Vector3 currentPos = startOffset;
-        Vector3 currentVelocity = _force * Camera.main.transform.forward / mass;
+        Vector3 currentVelocity = ForceVector / mass;
 
         for(int i = 0; i < _linePoints; i++)
         {
