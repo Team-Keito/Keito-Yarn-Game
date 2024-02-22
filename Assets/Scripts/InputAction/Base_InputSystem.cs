5e97a43bb3ca11e88ac855a61436b7d403f1a006
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-02-21 18:59:47 -0800

    Re-added Base_InputSystem for backwards compatability

---
+++ Assets/Scripts/InputAction/Base_InputSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Base_InputSystem : MonoBehaviour
+{
+    protected PlayerControls _input = InputManager.Input;
+}
