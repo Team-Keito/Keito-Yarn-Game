f269caa0b368eab82470be7204616d16876de3b8
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-02-23 10:29:37 -0800

    Added In UI branch & changed Main UI buttons to be transparent

--- Assets/Scripts/UI/RandomBallSpawner.cs
+++ Assets/Scripts/UI/RandomBallSpawner.cs
@@ -11,13 +11,14 @@ public class RandomBallSpawner : MonoBehaviour
     [SerializeField] float _force = 2f;
 
     private Vector3 _target;
+    private float time = 0;
 
     private void Start()
     {
         transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
 
         Vector3 offset = transform.forward * Random.Range(0, _radius);
-        time = _delay;
+        //time = _delay;
 
         _target = transform.position;
         _target.y = 0;
@@ -29,13 +30,7 @@ public class RandomBallSpawner : MonoBehaviour
         _target.y = 0;
     }
 
-    void CullingEvent(CullingGroupEvent sphere)
-    {
-        Debug.Log(sphere);
-    }
-
-
-    private float time = 0;
+    
 
     // Update is called once per frame
     void Update()
@@ -44,22 +39,23 @@ public class RandomBallSpawner : MonoBehaviour
 
         if(time < 0)
         {
-            _spawner.transform.localPosition = new Vector3(Random.Range(-_radius, _radius), transform.position.y, Random.Range(-_radius, _radius));
-            _spawner.transform.LookAt(_target);
-
-
-            int RandInt = Random.Range(0, _prefabs.Length);
-            GameObject go = Instantiate(_prefabs[RandInt], _spawner.transform.position, Random.rotation);
-
-            DisableComps(go);
-            Rigidbody rb = go.GetComponent<Rigidbody>();
-
-            rb.AddForce(_spawner.transform.forward * _force, ForceMode.Impulse);
-
+            SpawnRandom();
             time = _delay;
         }
+    }
 
+    private void SpawnRandom()
+    {
+        _spawner.transform.localPosition = new Vector3(Random.Range(-_radius, _radius), transform.position.y, Random.Range(-_radius, _radius));
+        _spawner.transform.LookAt(_target);
 
+        int RandInt = Random.Range(0, _prefabs.Length);
+        GameObject go = Instantiate(_prefabs[RandInt], _spawner.transform.position, Random.rotation);
+
+        DisableComps(go);
+        Rigidbody rb = go.GetComponent<Rigidbody>();
+
+        rb.AddForce(_spawner.transform.forward * _force, ForceMode.Impulse);
     }
 
     private void DisableComps(GameObject go)
