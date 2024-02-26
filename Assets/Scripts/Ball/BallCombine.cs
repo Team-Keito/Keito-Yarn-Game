85a08fc2d7982490eba6d835afad2a67270beb55
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-02-25 18:23:58 -0800

    Added Glow Effect & connected to OnMaxSize

--- Assets/Scripts/Ball/BallCombine.cs
+++ Assets/Scripts/Ball/BallCombine.cs
@@ -55,7 +55,7 @@ public class BallCombine : MonoBehaviour
             float combinedMass = _rigidBody.mass + collision.rigidbody.mass * _massMultiplier;
             
 
-            if (transform.localScale.x >= _scaleVectorCap.x || _rigidBody.mass >= _massCap)
+            if (transform.localScale.x >= _scaleVectorCap.x * 0.99f || _rigidBody.mass >= _massCap)
             {
                 return;            
             }
