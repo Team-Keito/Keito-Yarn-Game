28155e86ffb7d4d2f1a7233343561a4bdb8337eb
Samuel325 <ayenisamuel87@gmail.com>
2024-02-26 13:42:26 -0500

    TestNextColorDisplay conflict
    There was a merge conflict with "TestNextColorDisplay" missing from my branch. I moved the code from "TestNextColorDisplay" to "NextColorDisplay" and deleted the missing script on my branch.

--- Assets/Scripts/UI/NextColorUI.cs
+++ Assets/Scripts/UI/NextColorUI.cs
@@ -7,14 +7,49 @@ public class NextColorUI : MonoBehaviour
 {
     [SerializeField] SlingShot _slingshot; //probably convert to parent class?
 
-    [SerializeField] RawImage[] _Colors;
+    [SerializeField] GameObject yarnBall;
+
+    private LinkedList<GameObject> _Colors;
 
     public void UpdateNextColor(Queue<Color> Colors)
     {
-        int count = 0;
-        foreach(Color color in Colors)
+        if (_Colors.Count > 2)
         {
-            _Colors[count++].color = color;
+            StartCoroutine(PlayAnimation(Colors));
+        }
+        else
+        {
+            // Add the first three yarn balls
+            foreach (Color color in Colors)
+            {
+                yarnBall = Instantiate(yarnBall, transform);
+
+                yarnBall.GetComponent<RawImage>().color = color;
+
+                _Colors.AddLast(yarnBall);
+            }
+        }
+    }
+
+    IEnumerator PlayAnimation(Queue<Color> Colors)
+    {
+        // Play the fade animation
+        _Colors.First.Value.GetComponent<Animator>().Play("Fade");
+
+        yield return new WaitForSeconds(_Colors.First.Value.GetComponent<Animator>().runtimeAnimatorController.animationClips[1].length);
+
+        yarnBall = Instantiate(yarnBall, transform);
+
+        // Add the yarn ball to the list and set its color
+        yarnBall.GetComponent<RawImage>().color = Colors.Peek();
+
+        _Colors.AddLast(yarnBall);
+
+        // Removes extra yarn ball UI's that are caused when the player rapidly throws the yarn balls into the level
+        while (_Colors.Count > 3)
+        {
+            Destroy(_Colors.First.Value);
+            _Colors.RemoveFirst();
         }
     }
 }
