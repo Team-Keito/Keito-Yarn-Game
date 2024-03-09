4ad050b3310845240ff025911be8a8a372e355d3
Samuel325 <ayenisamuel87@gmail.com>
2024-03-09 18:22:46 -0500

    Added cat fact scriptable object

---
+++ Assets/Scripts/ScriptableObjects/UI/CatFactSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cat Fact", menuName = "SO/Cat Fact")]
+public class CatFactSO : ScriptableObject
+{
+    public string title;
+    public string factText;
+}
