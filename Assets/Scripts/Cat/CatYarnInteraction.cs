68ea92724d7ddcefe684240759d7d0f559a74e74
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-03-17 14:27:42 -0700

    Added toggle for Cat to Reject Damaged ball

--- Assets/Scripts/Cat/CatYarnInteraction.cs
+++ Assets/Scripts/Cat/CatYarnInteraction.cs
@@ -13,9 +13,12 @@ public class CatYarnInteraction : MonoBehaviour
     [SerializeField, Tooltip("The minimum square velocity at which the cat will reject the yarn ball.\nNOTE: this is velocity^2")]
     private float _minSqrVelocityRejection = 4;
 
+    [SerializeField] private bool _rejectDamagedBall = true;
+
     public UnityEvent<float, bool> OnCatScored;
     public UnityEvent OnRejectBallSize;
     public UnityEvent OnRejectBallForce;
+    public UnityEvent OnRejectDamagedBall;
     public UnityEvent<ColorSO> OnFavoriteColor;
 
     private ColorSO _favoriteColor;
@@ -62,10 +65,22 @@ public class CatYarnInteraction : MonoBehaviour
     {
         ColorController colorHit = collision.gameObject.GetComponent<ColorController>();
 
+        if(_rejectDamagedBall && colorHit.isDamaged())
+        {
+            RejectDamagedBall();
+            return;
+        }
+
         bool isFavorite = _favoriteColor == colorHit.Color && !colorHit.isDamaged();
 
         OnCatScored.Invoke(collision.transform.localScale.x, isFavorite);
         Destroy(collision.gameObject);
+    }
+
+    private void RejectDamagedBall()
+    {
+        //TODO: Add in damaged ball thought bubble?
+        OnRejectDamagedBall.Invoke();
     }
 
     private void RejectBallSize(Collision collision)
