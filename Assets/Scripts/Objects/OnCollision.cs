ba94f407fd5835fdf308ab536ef8c294cf4373ba
JasonA <149907110+jyamaAsagohan@users.noreply.github.com>
2024-03-20 19:26:42 -0700

    Moved Collision script & added to namespace

---
+++ Assets/Scripts/Objects/OnCollision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ComponentHelper
+{
+    public class OnCollision : MonoBehaviour
+    {
+        public UnityEvent OnCollisionEvent;
+        public TagSO _tag;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (_tag.Compare(collision.gameObject))
+            {
+                OnCollisionEvent.Invoke();
+            }
+        }
+    }
+
+}
